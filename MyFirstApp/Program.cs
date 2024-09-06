9150a31c0b92380c3ca319d04f83391499943d32
Mathias Vapper. <mathias.vapper@tktk.ee>
2024-09-06 15:58:40 +0300

    create new app

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+//rakendus küsib kasutajal sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+Console.WriteLine("enter your name:");  //output
+//string - sõne
+string userName = Console.ReadLine();  //input
+
+
+Console.WriteLine("Hello" + " "+ userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello {userName}  !");
+
